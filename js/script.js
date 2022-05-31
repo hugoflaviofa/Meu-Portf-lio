c151abefda0b53f37dfa711e2d66d9ebb63c35d2
Hugo Flávio <hugoflavio@live.com>
2022-05-31 14:18:43 -0300

    Implementado ativação/animação do menu hamburguer ao clicar

--- js/script.js
+++ js/script.js
@@ -1,19 +1,7 @@
 const hanburguerButton = document.querySelector('#hamburguer-button');
-const navOptions = document.querySelectorAll('.containner-options p');
-// const navContainner = document.querySelector('.nav-containner');
-const addNav = document.querySelector('#add-nav');
+const navOptions = document.querySelector('.containner-options');
 
 hanburguerButton.addEventListener('click', () => {
-  addNav.classList.toggle('add-nav-click');
-  if (addNav.classList.contains('add-nav-click')) {
-    navOptions.forEach((valueArr) => {
-      let testeCreat = valueArr.cloneNode(true);
-      addNav.appendChild(testeCreat);
-      console.log(valueArr.innerHTML);
-    });
-  } else {
-    while (addNav.hasChildNodes()) {
-      addNav.removeChild(addNav.firstChild);
-    }
-  }
+  navOptions.classList.toggle('active')
+
 })
